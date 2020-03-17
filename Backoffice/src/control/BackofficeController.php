ee417f11e84401304951e0fdb54683604ce7403b
sehbaoui <mouad>
2020-03-17 19:21:28 +0100

    premier commit

---
+++ Backoffice/src/control/BackofficeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace lbs\command\control;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use lbs\command\model\Item as item;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use lbs\command\model\Commande as commande;
+use Ramsey\Uuid\Uuid;
+use GuzzleHttp\Client;
+
+class BackofficeController
+{
+    protected $c;
+
+    public function __construct(\Slim\Container $c = null)
+    {
+        $this->c = $c;
+    }
+
+    public function test(Request $req, Response $resp, array $args)
+    {
+        $rs = $resp->withStatus(200)
+            ->withHeader('Content-Type', 'application/json;charset=utf-8');
+        $rs->getBody()->write(json_encode("test"));
+        return $rs;
+    }
+}
---
+++ Player/api/index.php
@@ -0,0 +1,30 @@
+<?php
+require '../src/vendor/autoload.php';
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use \lbs\common\bootstrap\Eloquent;
+use \DavidePastore\Slim\Validation\Validation as Validation;
+
+$config = parse_ini_file("../src/conf/conf.ini");
+$db = new Illuminate\Database\Capsule\Manager();
+$db->addConnection($config);
+$db->setAsGlobal();
+$db->bootEloquent();
+
+$errors = require '../src/commons/errors/errors.php';
+$configuration = new \Slim\Container(['settings' => ['displayErrorDetails' => true]]);
+$app_config = array_merge($errors);
+$app = new \Slim\App([
+    'settings' => [
+        'displayErrorDetails' => true,
+        'debug' => true,
+        'whoops.editor' => 'sublime',
+    ]]);
+
+
+$app->get('/test[/]', function ($rq, $rs, $args) {
+    return (new lbs\command\control\PlayerController($this))->test($rq, $rs, $args);
+});
+
+$app->run();
