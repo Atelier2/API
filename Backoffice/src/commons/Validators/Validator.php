ad092778210c096840f3ffae2b35788439c01fb1
sehbaoui <mouad>
2020-03-21 14:24:10 +0100

    modification validators , affichage erreurs validators

--- Backoffice/src/commons/Validators/Validator.php
+++ Backoffice/src/commons/Validators/Validator.php
@@ -6,18 +6,42 @@ use \Respect\Validation\Validator as v;
 
 class Validator
 {
-    public static function validators()
+    public static function validatorsUsers()
     {
         return
             [
-                'firstname' => v::StringType()->alpha(),
-                'lastname' => v::StringType()->alpha(),
-                'email' => v::email(),
-                'phone' => v::phone(),
-                'street_number' => v::intVal(),
-                'street' => v::StringType()->alpha(),
-                'city' => v::stringType()->alpha(),
-                'zip_code' => v::intVal(),
+                'firstname' => v::StringType()->notEmpty()->alpha(),
+                'lastname' => v::StringType()->notEmpty()->alpha(),
+                'email' => v::email()->notEmpty(),
+                'phone' => v::phone()->notEmpty(),
+                'street_number' => v::intType()->notEmpty(),
+                'street' => v::StringType()->notEmpty()->alpha(),
+                'city' => v::stringType()->notEmpty()->alpha(),
+                'zip_code' => v::intType()->notEmpty(),
+            ];
+    }
+
+    public static function validatorsSeries()
+    {
+        return
+            [
+                'city' => v::StringType()->notEmpty()->alpha(),
+                'distance' => v::intType()->notEmpty(),
+                'latitude' => v::StringType()->notEmpty()->alpha(),
+                'longitude' => v::StringType()->notEmpty()->alpha(),
+                'zoom' => v::intType()->notEmpty(),
+                'nb_pictures' => v::intVal()->notEmpty(),
+            ];
+    }
+
+    public static function validatorsPicture()
+    {
+        return
+            [
+                'description' => v::StringType()->notEmpty()->alpha(),
+                'latitude' => v::notEmpty(),
+                'longitude' => v::notEmpty(),
+                'link' => v::url()->notEmpty(),
             ];
     }
 }
